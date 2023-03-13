32a24bd189018720fb2c431e46cca3124605e56f
AirHeaven <1229725808@qq.com>
2023-03-13 12:36:40 +0800

    refactor:reorganize the structure of frontend code

---
+++ frontend/src/components/Feed/FeedEntry.vue
@@ -0,0 +1,31 @@
+<template>
+  <FeedMenu></FeedMenu>
+  <v-list-item @click="getPostList">
+    <v-list-item-title class="left-align">{{ feed.title }}</v-list-item-title>
+    <v-list-item-subtitle
+      class="left-align"
+      v-html="feed.description"
+    ></v-list-item-subtitle>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import FeedMenu from "./FeedMenu.vue";
+
+const props = defineProps(["feed"]);
+
+const store = useStore();
+const router = useRouter();
+
+const getPostList = () => {
+  router.push("/feeds/" + props.feed.id);
+};
+</script>
+
+<style scoped>
+.left-align {
+  text-align: left;
+}
+</style>
---
+++ frontend/src/views/SideView.vue
@@ -0,0 +1,21 @@
+<template>
+  <primary-sidebar></primary-sidebar>
+</template>
+
+<script lang="ts" setup>
+import PrimarySidebar from "../components/Sidebar/PrimarySidebar.vue";
+</script>
+
+<style lang="scss" scoped>
+#sidebar-menu {
+  overflow-y: hidden;
+}
+
+.v-btn::before {
+  background-color: transparent;
+}
+
+.left-align {
+  text-align: left;
+}
+</style>
